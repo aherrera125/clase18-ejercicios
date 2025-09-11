bb547c9e8a7dd24d016cb053bb7ea066b34b5abf
aherrera125 <j.alejandroherrera@gmail.com>
2025-09-10 22:02:38 -0300

    Filter All tasks & Completed tasks & Pending tasks

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,6 +1,9 @@
 let tasks = [];
 let taskList = formData2.querySelector("#display");
 const button = document.getElementById("addTask");
+const allTasks = document.getElementById("allTasks");
+const completedTasks = document.getElementById("completedTasks");
+const pendingTasks = document.getElementById("pendingTasks");
 
 button.addEventListener("click", (event) => {
   event.preventDefault();
@@ -14,41 +17,129 @@ button.addEventListener("click", (event) => {
     completed: false,
   };
   tasks.push(taskObject);
-  renderizar();
+  renderizar("all");
 });
 
-function renderizar() {
+allTasks.addEventListener("change", (e) => {
+  renderizar("all");
+});
+
+completedTasks.addEventListener("change", (e) => {
+  renderizar("completed");
+});
+
+pendingTasks.addEventListener("change", (e) => {
+  renderizar("pending");
+});
+
+function renderizar(typeOp) {
   taskList.textContent = "";
-  tasks.forEach((task) => {
-    const li = document.createElement("li");
+  switch (typeOp) {
+    case "all":
+      tasks.forEach((task) => {
+        const li = document.createElement("li");
 
-    const chkCompleted = document.createElement("input");
-    chkCompleted.type = "radio";
-    chkCompleted.style.accentColor = "green";
-    chkCompleted.checked = task.completed;
+        const chkCompleted = document.createElement("input");
+        chkCompleted.type = "radio";
+        chkCompleted.style.accentColor = "green";
+        chkCompleted.checked = task.completed;
 
-    const btnDelete = document.createElement("button");
-    btnDelete.textContent = "X";
+        const btnDelete = document.createElement("button");
+        btnDelete.textContent = "X";
 
-    chkCompleted.addEventListener("change", () => {
-      task.completed = chkCompleted.checked;
-      renderizar();
-    });
+        chkCompleted.addEventListener("change", () => {
+          task.completed = chkCompleted.checked;
+          renderizar("all");
+        });
 
-    btnDelete.addEventListener("click", () => {
-      const index = tasks.indexOf(task);
-      if (index > -1) {
-        tasks.splice(index, 1);
-      }
-      renderizar();
-    });
+        btnDelete.addEventListener("click", () => {
+          const index = tasks.indexOf(task);
+          if (index > -1) {
+            tasks.splice(index, 1);
+          }
+          renderizar("all");
+        });
 
-    const span = document.createElement("span");
-    span.textContent = task.name;
+        const span = document.createElement("span");
+        span.textContent = task.name;
 
-    li.appendChild(chkCompleted);
-    li.appendChild(btnDelete);
-    li.appendChild(span);
-    taskList.appendChild(li);
-  });
+        li.appendChild(chkCompleted);
+        li.appendChild(btnDelete);
+        li.appendChild(span);
+        taskList.appendChild(li);
+      });
+      break;
+    case "completed":
+      tasks.forEach((task) => {
+        if (task.completed) {
+          const li = document.createElement("li");
+
+          const chkCompleted = document.createElement("input");
+          chkCompleted.type = "radio";
+          chkCompleted.style.accentColor = "green";
+          chkCompleted.checked = task.completed;
+
+          const btnDelete = document.createElement("button");
+          btnDelete.textContent = "X";
+
+          chkCompleted.addEventListener("change", () => {
+            task.completed = chkCompleted.checked;
+            renderizar("all");
+          });
+
+          btnDelete.addEventListener("click", () => {
+            const index = tasks.indexOf(task);
+            if (index > -1) {
+              tasks.splice(index, 1);
+            }
+            renderizar("all");
+          });
+
+          const span = document.createElement("span");
+          span.textContent = task.name;
+
+          li.appendChild(chkCompleted);
+          li.appendChild(btnDelete);
+          li.appendChild(span);
+          taskList.appendChild(li);
+        }
+      });
+      break;
+    case "pending":
+      tasks.forEach((task) => {
+        if (!task.completed) {
+          const li = document.createElement("li");
+
+          const chkCompleted = document.createElement("input");
+          chkCompleted.type = "radio";
+          chkCompleted.style.accentColor = "green";
+          chkCompleted.checked = task.completed;
+
+          const btnDelete = document.createElement("button");
+          btnDelete.textContent = "X";
+
+          chkCompleted.addEventListener("change", () => {
+            task.completed = chkCompleted.checked;
+            renderizar("all");
+          });
+
+          btnDelete.addEventListener("click", () => {
+            const index = tasks.indexOf(task);
+            if (index > -1) {
+              tasks.splice(index, 1);
+            }
+            renderizar("all");
+          });
+
+          const span = document.createElement("span");
+          span.textContent = task.name;
+
+          li.appendChild(chkCompleted);
+          li.appendChild(btnDelete);
+          li.appendChild(span);
+          taskList.appendChild(li);
+        }
+      });
+      break;
+  }
 }
